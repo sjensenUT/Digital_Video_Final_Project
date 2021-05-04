e0efa8d9b321d9d5fa3ef6e83bad13884fe6dd7f
ashanuka <ashen9436@gmail.com>
2021-05-04 18:57:46 -0500

    denoiser for real data

--- denoiser.py
+++ denoiser.py
@@ -13,14 +13,16 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Image De-Noising')
 parser.add_argument('--inference', action="store_true", default=False)
-parser.add_argument('--checkpoint_path', type=str, default='./model/1.ckpt')
+parser.add_argument('--realdata', action="store_true", default=False)
+parser.add_argument('--checkpoint_path', type=str, default='./model/2.ckpt')
 parser.add_argument('--num_epochs', type=int, default=10)
-parser.add_argument('--batch_size', type=int, default=50)
-parser.add_argument('--image_dir', type=str, default='./images_2')
+parser.add_argument('--batch_size', type=int, default=10)
+parser.add_argument('--image_dir', type=str, default='./real-data')
 #parser.add_argument('--learning_rate', type=float, default=0.001)
 args = parser.parse_args()
 
 inference = args.inference
+realdata = args.realdata
 checkpoint_path = args.checkpoint_path
 num_epochs = args.num_epochs
 batch_size = args.batch_size
@@ -38,6 +40,9 @@ def load_images_from_folder(folderGT, folderNoisy):
         imgNoisy = Image.open(os.path.join(folderNoisy,filename))
         imgDataGT = np.asarray(imgGT)
         imgDataNoisy = np.asarray(imgNoisy)
+        if inference and realdata:
+            imgDataGT = imgDataGT[0:384]
+            imgDataNoisy = imgDataNoisy[0:384]
         imgTensorGT = tf.convert_to_tensor(imgDataGT, dtype=tf.float32)
         imgTensorNoisy = tf.convert_to_tensor(imgDataNoisy, dtype=tf.float32)
         imgTensorsGT.append(imgTensorGT)
@@ -45,49 +50,6 @@ def load_images_from_folder(folderGT, folderNoisy):
         if(count==5186): break
     return [tf.stack(imgTensorsGT)/255.0, tf.stack(imgTensorsNoisy)/255.0] 
         
-
-print('----------loading images----------')
-[x_train, x_train_noisy] = load_images_from_folder(os.path.join(image_dir, '512x384'), os.path.join(image_dir, '512x384_noisy'))
-print('')
-print('ground truth shape = ' + str(x_train.shape))
-print('noisy shape = ' + str(x_train_noisy.shape))
-
-#rand = random.randint(0,1000000)
-#ground_truth_images = tf.random.shuffle(ground_truth_images, seed=rand)
-#noisy_images = tf.random.shuffle(noisy_images, seed=rand)
-
-
-train_size = int(x_train.shape[0]*0.90)
-test_size = x_train.shape[0] - train_size
-
-print('train_size = ' + str(train_size))
-print('test_size = ' + str(test_size))
-
-[x_train, x_test] = tf.split(x_train, [train_size, test_size], axis=0)
-[x_train_noisy, x_test_noisy] = tf.split(x_train_noisy, [train_size, test_size], axis=0)
-
-print('train gt shape = ' + str(x_train.shape))
-print('test gt shape = ' + str(x_test.shape))
-print('train noisy shape = ' + str(x_train_noisy.shape))
-print('test noisy shape = ' + str(x_test_noisy.shape))
-
-#if inference:
-#    x_test_noisy = x_test_noisy[0:99]
-#    x_test = x_test[0:99]
-
-#n = 5
-#plt.figure(figsize=(20, 8))
-#plt.gray()
-#for i in range(n):
-#    ax = plt.subplot(2, n, i + 1) 
-#    plt.title("original", size=10) 
-#    plt.imshow(tf.squeeze(x_test[i])) 
-#    plt.gray() 
-#    bx = plt.subplot(2, n, n+ i + 1) 
-#    plt.title("original + noise", size=10) 
-#    plt.imshow(tf.squeeze(x_test_noisy[i])) 
-#plt.show()
-
 class NoiseReducer(tf.keras.Model):
   def __init__(self):
 
@@ -139,6 +101,36 @@ class NoiseReducer2(tf.keras.Model):
     return decoded
 
 
+if inference:
+    print('----------loading images for inference----------')
+    #[x_test, x_test_noisy] = load_images_from_folder(os.path.join(image_dir, '512x384_validation'), os.path.join(image_dir, '512x384_noisy_validation'))
+    image_dir = os.path.join(image_dir, 'validation')
+    [x_test, x_test_noisy] = load_images_from_folder(os.path.join(image_dir, 'ground_truths'), os.path.join(image_dir, 'noisy'))
+    print('')
+    print('ground truth shape = ' + str(x_test.shape))
+    print('noisy shape = ' + str(x_test_noisy.shape))
+else:
+    print('----------loading images for training----------')
+    [x_train, x_train_noisy] = load_images_from_folder(os.path.join(image_dir, '512x384'), os.path.join(image_dir, '512x384_noisy'))
+    print('')
+    print('ground truth shape = ' + str(x_train.shape))
+    print('noisy shape = ' + str(x_train_noisy.shape))
+    
+    train_size = int(x_train.shape[0]*0.90)
+    test_size = x_train.shape[0] - train_size
+    
+    print('train_size = ' + str(train_size))
+    print('test_size = ' + str(test_size))
+    
+    [x_train, x_test] = tf.split(x_train, [train_size, test_size], axis=0)
+    [x_train_noisy, x_test_noisy] = tf.split(x_train_noisy, [train_size, test_size], axis=0)
+    
+    print('train gt shape = ' + str(x_train.shape))
+    print('test gt shape = ' + str(x_test.shape))
+    print('train noisy shape = ' + str(x_train_noisy.shape))
+    print('test noisy shape = ' + str(x_test_noisy.shape))
+
+
 autoencoder = NoiseReducer2()
 autoencoder.compile(optimizer='adam', loss='mse')
 
@@ -161,12 +153,17 @@ else:
             callbacks=[cp_callback])
 
 
-#encoded_imgs=autoencoder.encoder(x_test_noisy).numpy()
-#decoded_imgs=autoencoder.decoder(encoded_imgs)
-
-#decoded_imgs=autoencoder.call(x_test_noisy)
 decoded_imgs=autoencoder.predict(x_test_noisy, batch_size=batch_size)
+real_psnr = tf.image.psnr(x_test_noisy, x_test, max_val=1.0)
+model_psnr1 = tf.image.psnr(decoded_imgs, x_test, max_val=1.0)
+avg_orig_psnr = tf.math.reduce_mean(real_psnr)
+avg_recon_psnr = tf.math.reduce_mean(model_psnr1)
+avg_orig_psnr = avg_orig_psnr.numpy() 
+avg_recon_psnr = avg_recon_psnr.numpy()
+print('Original Average PSNR ' + str(avg_orig_psnr))
+print('Reconstructed Average PSNR '+ str(avg_recon_psnr))
 
+"""
 n = 10 
 plt.figure(figsize=(20, 7))
 plt.gray()
@@ -192,4 +189,7 @@ for i in range(n):
     ax.get_xaxis().set_visible(False) 
     ax.get_yaxis().set_visible(False) 
 
-plt.savefig('final.png')
+plt.savefig('real-final.png')
+"""
+
+
